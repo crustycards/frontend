cf63ce51122dabe2fbf3f08f001a1460fc7ad7d4
Thomas Volk <tvolk131@gmail.com>
2017-08-12 23:50:18 -0700

    Client homepage dynamically renders friends and friend requests

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,18 +12,46 @@ class Home extends React.Component {
     this.socket = io();
     this.socket.on('friendrequestsend', (data) => {
       console.log(JSON.parse(data));
+      let users = JSON.parse(data);
+      let otherUser;
+      if (users.friender.id === this.state.currentUser.id) {
+        otherUser = users.friendee;
+        this.addFriendRequestSent(otherUser);
+      } else {
+        otherUser = users.friender;
+        this.addFriendRequestReceived(otherUser);
+      }
     });
     this.socket.on('friendrequestaccept', (data) => {
       console.log(JSON.parse(data));
+      let users = JSON.parse(data);
+      let otherUser;
+      if (users.acceptor.id === this.state.currentUser.id) {
+        otherUser = users.acceptee;
+      } else {
+        otherUser = users.acceptor;
+      }
+      this.removeFriendRequestSent(otherUser);
+      this.removeFriendRequestReceived(otherUser);
+      this.addFriend(otherUser);
     });
     this.socket.on('unfriend', (data) => {
-      console.log(JSON.parse(data));
+      let users = JSON.parse(data);
+      let otherUser;
+      if (users.unfriender.id === this.state.currentUser.id) {
+        otherUser = users.unfriendee;
+      } else {
+        otherUser = users.unfriender;
+      }
+      this.removeFriendRequestSent(otherUser);
+      this.removeFriendRequestReceived(otherUser);
+      this.removeFriend(otherUser);
     });
     this.state = {
       currentUser: null,
       friends: [],
       requestsSent: [],
-      requestReceived: []
+      requestsReceived: []
     };
     axios.get('/api/currentuser')
     .then((response) => {
@@ -35,8 +63,47 @@ class Home extends React.Component {
       let friendData = response.data;
       this.setState({friends: friendData.friends});
       this.setState({requestsSent: friendData.requestsSent});
-      this.setState({requestReceived: friendData.requestsReceived});
+      this.setState({requestsReceived: friendData.requestsReceived});
     });
+  }
+
+  addFriend (friend) {
+    this.state.friends.push(friend);
+    this.forceUpdate();
+  }
+  removeFriend (friend) {
+    for (let i = 0; i < this.state.friends.length; i++) {
+      if (this.state.friends[i].email === friend.email) {
+        this.state.friends.splice(i, 1);
+        this.forceUpdate();
+      }
+    }
+  }
+  addFriendRequestSent (friend) {
+    this.state.requestsSent.push(friend);
+    this.forceUpdate();
+  }
+  removeFriendRequestSent (friend) {
+    console.log('BOOP');
+    for (let i = 0; i < this.state.requestsSent.length; i++) {
+      if (this.state.requestsSent[i].email === friend.email) {
+        this.state.requestsSent.splice(i, 1);
+        this.forceUpdate();
+      }
+    }
+  }
+  addFriendRequestReceived (friend) {
+    this.state.requestsReceived.push(friend);
+    this.forceUpdate();
+  }
+  removeFriendRequestReceived (friend) {
+    console.log('BOOP');
+    for (let i = 0; i < this.state.requestsReceived.length; i++) {
+      if (this.state.requestsReceived[i].email === friend.email) {
+        this.state.requestsReceived.splice(i, 1);
+        this.forceUpdate();
+      }
+    }
   }
 
   render() {
@@ -46,7 +113,7 @@ class Home extends React.Component {
         <div>{this.state.currentUser ? this.state.currentUser.firstname + ' ' + this.state.currentUser.lastname : 'Loading...'}</div>
         <FriendsList friends={this.state.friends} />
         <FriendRequestsSent requestsSent={this.state.requestsSent} />
-        <FriendRequestsReceived requestsReceived={this.state.requestReceived} />
+        <FriendRequestsReceived requestsReceived={this.state.requestsReceived} />
         <FrienderPanel />
       </div>
     ) 
