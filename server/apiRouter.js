09cddb49ce8694ae2045df1604dfbe706432ab54
Thomas Volk <tvolk131@gmail.com>
2017-08-12 22:10:07 -0700

    API router has no duplicate routes

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -50,53 +50,15 @@ module.exports = (socketHandler) => {
   router.delete('/friends', auth.isLoggedIn, (req, res) => {
     db.removeFriend(req.user.email, req.query.user)
     .then(() => {
-      res.send(JSON.stringify({message: 'success'}));
+      socketHandler.respondByUserEmail([req.user.email, req.query.user], 'unfriend', {
+        unfriender: req.user.email,
+        unfriendee: req.query.user
+      });
+      res.send(JSON.stringify({message: 'successsss'}));
     })
-    .catch((error) => {
-      res.send(JSON.stringify({error}));
-    });
-  });
-
-  // Expects another user's email in req.body.userEmail
-  // and then adds a friend request with that particular user
-  router.post('/friends', auth.isLoggedIn, (req, res) => {
-    var friender = req.user;
-    var friendee;
-    db.getUser({email: req.body.userEmail}).then((user) => {
-      friendee = user;
-    }).then(() => {
-      return db.addFriend(friender.id, friendee.id, 'create');
-    }).catch((err) => {
-      res.send('The email you entered is not linked to an existing user');
-    }).then((data) => {
-      if (data) {
-        res.send('Friend request sent');
-      } else {
-        res.send('Something went wrong when submitting friend request');
-      }
-    });
-  });
-  // TODO - Link these together
-  router.post('/friends', auth.isLoggedIn, (req, res) => {
-    var friender = req.user;
-    var friendee;
-    db.getUser({id: req.body.friendId}).then((user) => {
-      friendee = user;
-      return db.addFriend(friender.id, friendee.id, 'accept')
-    }).then(() => {
-      res.send('Friend request accepted');
-    });
-  });
-
-  router.delete('/friends', auth.isLoggedIn, (req, res) => {
-    var unfriender = req.user;
-    var unfriendee;
-    db.getUser({id: req.body.friendId}).then((user) => {
-      unfriendee = user;
-      db.removeFriend(unfriender.id, unfriendee.id)
-    }).then(() => {
-      res.send('Friend successfully removed');
-    });
+    // .catch((error) => {
+    //   res.send(JSON.stringify({error}));
+    // });
   });
 
   router.get('/*', (req, res) => {
