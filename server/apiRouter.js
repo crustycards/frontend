7bfffe9fa6a80c3bfebfb68349056b2b90d17125
Thomas Volk <tvolk131@gmail.com>
2017-08-14 20:08:14 -0700

    API router is cleaner

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -10,85 +10,92 @@ module.exports = (socketHandler) => {
     res.send(JSON.stringify(req.user));
   });
 
-  router.get('/messages', auth.isLoggedIn, (req, res) => {
-    // Get a list of messages with a particular user
-    db.getMessages(req.user.email, req.query.user)
-    .then(JSON.stringify)
-    .then(res.send)
-    .catch((error) => {
-      res.send(JSON.stringify({error}));
-    });
-  });
-
-  router.get('/friends', auth.isLoggedIn, (req, res) => {
-    // Get a list of all friends and pending friend requests
-    db.getFriendData(req.user.email).then(JSON.stringify).then((data) => {
-      res.send(data);
-    });
-  });
-  router.post('/friends', auth.isLoggedIn, (req, res) => {
-    if (req.body.type === 'request') {
-      db.sendFriendRequest(req.user.email, req.body.user)
-      .then(() => {
-        return db.getUser(req.body.user)
-        .then((friendee) => {
-          return {friender: req.user, friendee};
-        });
-      })
-      .then((users) => {
-        socketHandler.respondByUserEmail([users.friender.email, users.friendee.email], 'friendrequestsend', {
-          friender: users.friender,
-          friendee: users.friendee
-        });
-        res.send(JSON.stringify({message: 'success'}));
-      })
+  router.route('/messages')
+    .get(auth.isLoggedIn, (req, res) => {
+      // Get a list of messages with a particular user
+      db.getMessages(req.user.email, req.query.user)
+      .then(JSON.stringify)
+      .then(res.send)
       .catch((error) => {
         res.send(JSON.stringify({error}));
-      });
-    } else if (req.body.type === 'accept') {
-      db.acceptFriendRequest(req.user.email, req.body.user)
-      .then(() => {
-        return db.getUser(req.body.user)
-        .then((acceptee) => {
-          return {acceptor: req.user, acceptee};
-        });
-      })
-      .then((users) => {
-        socketHandler.respondByUserEmail([users.acceptor.email, users.acceptee.email], 'friendrequestaccept', {
-          acceptor: users.acceptor,
-          acceptee: users.acceptee
-        });
-        res.send(JSON.stringify({message: 'success'}));
-      })
-      .catch((error) => {
-        res.send(JSON.stringify({error}));
-      });
-    } else {
-      res.send(JSON.stringify({error: 'Did not specify whether a friend request was sent or accepted'}));
-    }
-  });
-  router.delete('/friends', auth.isLoggedIn, (req, res) => {
-    db.removeFriend(req.user.email, req.query.user)
-    .then(() => {
-      return db.getUser(req.query.user)
-      .then((unfriendee) => {
-        return {unfriender: req.user, unfriendee};
       });
     })
-    .then((users) => {
-      socketHandler.respondByUserEmail([users.unfriender.email, users.unfriendee.email], 'unfriend', {
-        unfriender: users.unfriender,
-        unfriendee: users.unfriendee
-      });
+    .post(auth.isLoggedIn, (req, res) => {
+      // TODO - Implement this
+      console.log(req.body);
       res.send(JSON.stringify({message: 'success'}));
-    })
-    .catch((error) => {
-      res.send(JSON.stringify({error}));
     });
-  });
+
+  router.route('/friends')
+    .get(auth.isLoggedIn, (req, res) => {
+      // Get a list of all friends and pending friend requests
+      db.getFriendData(req.user.email).then(JSON.stringify).then((data) => {
+        res.send(data);
+      });
+    })
+    .post(auth.isLoggedIn, (req, res) => {
+      if (req.body.type === 'request') {
+        db.sendFriendRequest(req.user.email, req.body.user)
+        .then(() => {
+          return db.getUser(req.body.user)
+          .then((friendee) => {
+            return {friender: req.user, friendee};
+          });
+        })
+        .then((users) => {
+          socketHandler.respondByUserEmail([users.friender.email, users.friendee.email], 'friendrequestsend', {
+            friender: users.friender,
+            friendee: users.friendee
+          });
+          res.send(JSON.stringify({message: 'success'}));
+        })
+        .catch((error) => {
+          res.send(JSON.stringify({error}));
+        });
+      } else if (req.body.type === 'accept') {
+        db.acceptFriendRequest(req.user.email, req.body.user)
+        .then(() => {
+          return db.getUser(req.body.user)
+          .then((acceptee) => {
+            return {acceptor: req.user, acceptee};
+          });
+        })
+        .then((users) => {
+          socketHandler.respondByUserEmail([users.acceptor.email, users.acceptee.email], 'friendrequestaccept', {
+            acceptor: users.acceptor,
+            acceptee: users.acceptee
+          });
+          res.send(JSON.stringify({message: 'success'}));
+        })
+        .catch((error) => {
+          res.send(JSON.stringify({error}));
+        });
+      } else {
+        res.send(JSON.stringify({error: 'Did not specify whether a friend request was sent or accepted'}));
+      }
+    })
+    .delete(auth.isLoggedIn, (req, res) => {
+      db.removeFriend(req.user.email, req.query.user)
+      .then(() => {
+        return db.getUser(req.query.user)
+        .then((unfriendee) => {
+          return {unfriender: req.user, unfriendee};
+        });
+      })
+      .then((users) => {
+        socketHandler.respondByUserEmail([users.unfriender.email, users.unfriendee.email], 'unfriend', {
+          unfriender: users.unfriender,
+          unfriendee: users.unfriendee
+        });
+        res.send(JSON.stringify({message: 'success'}));
+      })
+      .catch((error) => {
+        res.send(JSON.stringify({error}));
+      });
+    });
 
   router.get('/*', (req, res) => {
-    res.send();
+    res.send('Invalid api request');
   });
 
   return router;
