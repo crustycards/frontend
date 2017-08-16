d1b4ae526af37061f40c911c52e7d9b0ba0e8c74
Thomas Volk <tvolk131@gmail.com>
2017-08-15 21:22:59 -0700

    Passes tests again

--- spec/game.js
+++ spec/game.js
@@ -1,6 +1,6 @@
 const expect = require('chai').use(require('chai-as-promised')).expect;
 const Game = require('../game/Game.js');
-const Users = require('../game/users.js');
+const Users = require('../game/Users.js');
 
 module.exports.run = () => {
   describe('Users', () => {
