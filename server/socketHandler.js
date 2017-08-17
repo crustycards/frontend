3910c8ab2908203c81511a67cde56fc79dd38b71
Thomas Volk <tvolk131@gmail.com>
2017-08-17 11:56:42 -0700

    Server can handle having multiple sockets open for a single user

--- server/socketHandler.js
+++ server/socketHandler.js
@@ -3,7 +3,8 @@ let sockets = {};
 module.exports.openSocket = (socket) => {
   console.log('A user has connected');
   if (socket.request.user.email) {
-    sockets[socket.request.user.email] = socket;
+    sockets[socket.request.user.email] = sockets[socket.request.user.email] || [];
+    sockets[socket.request.user.email].push(socket);
   }
   // Add event listeners here
 };
@@ -11,15 +12,21 @@ module.exports.openSocket = (socket) => {
 module.exports.closeSocket = (socket) => {
   console.log('A user has disconnected');
   if (socket.request.user.id) {
-    delete sockets[socket.request.user.id];
+    for (let i = 0; i < sockets[socket.request.user.email].length; i++) {
+      if (sockets[socket.request.user.email][i] === socket) {
+        sockets[socket.request.user.email].splice(i, 1);
+        console.log('Deleted a socket');
+      }
+    }
   }
 };
 
 module.exports.respondByUserEmail = (userEmailArray, dataType, data) => {
-  for (let emailKey in sockets) {
-    for (let i = 0; i < userEmailArray.length; i++) {
-      if (userEmailArray[i] === emailKey) {
-        sockets[emailKey].emit(dataType, JSON.stringify(data));
+  for (let i = 0; i < userEmailArray.length; i++) {
+    // If this user has any open socket connections
+    if (sockets[userEmailArray[i]]) {
+      for (let j = 0; j < sockets[userEmailArray[i]].length; j++) {
+        sockets[userEmailArray[i]][j].emit(dataType, JSON.stringify(data));
       }
     }
   }
