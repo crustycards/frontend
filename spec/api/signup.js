668f83d57a193e6267e6872ad85d0dde27c6e3d0
Thomas Volk <tvolk131@gmail.com>
2017-09-04 15:46:50 -0700

    API tests are divided into separate files by endpoints

---
+++ spec/api/signup.js
@@ -0,0 +1,34 @@
+const chai = require('chai').use(require('chai-as-promised')).use(require('chai-http'));
+const expect = chai.expect;
+const app = require('../../server').listen(8080);
+const db = require('../../database');
+const request = chai.request(app);
+const sinon = require('sinon');
+const agent = chai.request.agent(app);
+const agent2 = chai.request.agent(app);
+
+describe('/signup', () => {
+  it('Should be able to create new local users', (done) => {
+    let tempAgent = chai.request.agent(app);
+    tempAgent.post('/signup')
+      .send({firstname: 'Hello', lastname: 'World', email: 'temp@agent.com', password: 'test'})
+      .then((res) => {
+        expect(res).to.have.cookie;
+        done();
+      });
+  });
+  it('Should return error when attempting to create a user using an email that is already taken', (done) => {
+    let tempAgentOne = chai.request.agent(app);
+    let tempAgentTwo = chai.request.agent(app);
+    tempAgentOne.post('/signup')
+      .send({firstname: 'Hello', lastname: 'World', email: 'temp@agent.com', password: 'test'})
+      .then(() => {
+        return tempAgentTwo.post('/signup')
+          .send({firstname: 'Hello', lastname: 'World', email: 'temp@agent.com', password: 'test'});
+      })
+      // TODO - Figure out a less janky way to do this
+      .catch(() => {
+        done();
+      });
+  });
+});
