c8bc302fc326dbe5dbc24316ae594d49f0bdd7ac
m <mgarber93@gmail.com>
2017-09-03 19:03:16 -0700

    Implement Tray Component

---
+++ client/src/components/Tray.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {GridList, GridTile} from 'material-ui/GridList';
+import Card from './COHCard.jsx';
+import {playCard} from '../store/modules/game';
+import { bindActionCreators } from 'redux';
+
+const Tray = ({hand, playCard}) => (
+  <div> 
+    <h4>Tray</h4>
+    <GridList
+      cols={4}
+      cellHeight={200}
+      padding={1}
+    > 
+      {hand.map(card => <Card 
+        key={card.id} 
+        card={card} 
+        playHandler={() => playCard(card)} 
+      />)}
+    </GridList>
+  </div>
+);
+
+const mapStateToProps = ({game}) => ({
+  hand: game.hand
+});
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  playCard
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Tray);
