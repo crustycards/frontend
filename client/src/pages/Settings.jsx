be9387eecda80e497bd2d1db9860dce822acd163
Thomas Volk <tvolk131@gmail.com>
2017-09-29 18:15:02 -0700

    Client code cleanup

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 
-class Settings extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render () {
-    return (<div>Settings</div>);
-  }
-}
+const Settings = (props) => (
+  <div>Settings</div>
+);
 
 export default Settings;
