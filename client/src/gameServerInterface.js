573f24ed080bb10e7c519cedb155fe9ae72fce4b
Thomas Volk <tvolk131@gmail.com>
2017-11-09 21:35:31 -0600

    Client has basic game server interface

--- client/src/gameServerInterface.js
+++ client/src/gameServerInterface.js
@@ -1,18 +1,95 @@
 import io from 'socket.io-client';
+import ax from 'axios';
+import store from './store';
+import { setGameState } from './store/modules/game';
 const gameServerURL = window.__PRELOADED_DATA__.gameURL;
+const axios = ax.create({
+  baseURL: gameServerURL,
+  timeout: 1000,
+  withCredentials: true
+});
 export const socket = io(gameServerURL);
 
+/**
+ * Creates a new game session
+ * @param {string} gameName Name of new game
+ * @param {number[]} cardpackIDs List of cardpacks to include in the game
+ * @returns {Promise} Resolves to the new game state, or rejects if there is an error
+ */
+export const createGame = (name, maxPlayers, cardpackIDs) => {
+  if (maxPlayers < 4 || maxPlayers > 20) {
+    return Promise.reject("Max players must be between 4 and 20");
+  }
+  return axios.post('/game/create', {name, maxPlayers, cardpackIDs})
+    .then((response) => {
+      store.dispatch(setGameState(response.data));
+      return parseGameData(response.data);
+    });
+};
 
-export const createGame = () => {};
+/**
+ * Adds user to the game with the given name
+ * @param {string} gameName The game name
+ */
+export const joinGame = (gameName) => {
+  return axios.post('/game/join', gameName);
+};
 
-export const joinGame = (gameName) => {};
+/**
+ * Leaves the current game
+ */
+export const leaveGame = () => {
+  return axios.post('/game/leave');
+};
 
-export const leaveGame = () => {};
+/**
+ * Fetches current game data
+ * @returns {Promise} Resolves to game data
+ */
+export const getGameState = () => {
+  return axios.get('/game/state')
+    .then((response) => {
+      return parseGameData(response.data);
+    });
+};
 
-export const getGameList = () => {};
+/**
+ * Fetches current game data
+ * @returns {Promise} Resolves to game data
+ */
+export const getGameList = () => {
+  return axios.get('/gamelist');
+};
 
-export const playCard = (cardID) => {};
+/**
+ * Plays a user's card if they are not the judge
+ * @param {number} cardID The ID of the card to play
+ * @returns {Promise} Resolves to an error (or null if it succeeded)
+ */
+export const playCard = (cardID) => {
+  return axios.post('/game/card', cardID);
+};
 
-export const kickPlayer = (playerID) => {};
+/**
+ * Kicks a player from the game if the kicker is the game owner
+ * @param {number} playerID
+ */
+export const kickPlayer = (playerID) => {
+  return axios.post('/game/kickplayer', playerID);
+};
 
-export const votePlayer = (playerID) => {};
+/**
+ * Used to vote a card for set of cards if you are the judge
+ * @param {number} cardID The ID of the card (or one of the cards in a set)
+ */
+export const vote = (cardID) => {
+  return axios.post('/game/vote');
+};
+
+
+const parseGameData = gameData => {
+  if (gameData.name === "") {
+    return null;
+  }
+  return gameData;
+};
