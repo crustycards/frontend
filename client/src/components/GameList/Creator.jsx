b317896eeae0b27eb5afb22be98b6a0cf58197fe
Thomas Volk <tvolk131@gmail.com>
2017-12-01 17:13:02 -0600

    Game creator component uses a MUI List for listing cardpacks

--- client/src/components/GameList/Creator.jsx
+++ client/src/components/GameList/Creator.jsx
@@ -4,6 +4,7 @@ import { createGame } from '../../gameServerInterface';
 import { connect } from 'react-redux';
 import { RaisedButton, TextField, Checkbox, DropDownMenu, MenuItem } from 'material-ui';
 import axios from 'axios';
+import { List, ListItem } from 'material-ui/List';
 import { setGameState } from '../../store/modules/game';
 
 class GameCreator extends Component {
@@ -40,11 +41,6 @@ class GameCreator extends Component {
   }
 
   render() {
-    const styles = {
-      checkbox: {
-        marginBottom: 16,
-      }
-    };
     return (
       <div>
         <div>
@@ -57,15 +53,18 @@ class GameCreator extends Component {
           <DropDownMenu maxHeight={300} value={this.state.maxPlayers} onChange={(event, index, value) => this.setState({maxPlayers: value})}>
             {this.maxPlayersDropdownItems}
           </DropDownMenu>
-          {this.props.cardpacks.map(c => (
-            <Checkbox
-              key={c.id}
-              label={c.name}
-              checked={this.state.cardpacksSelected.includes(c.id)}
-              onCheck={this.handleSelectChange.bind(this, c.id)}
-              style={styles.checkbox}
-            />
-          ))}
+          <List>
+            {this.props.cardpacks.map(c => (
+              <ListItem
+                key={c.id}
+                leftCheckbox={<Checkbox
+                  checked={this.state.cardpacksSelected.includes(c.id)}
+                  onCheck={this.handleSelectChange.bind(this, c.id)}
+                />}
+                primaryText={c.name}
+              />
+            ))}
+          </List>
           <RaisedButton type="submit" label="Submit" onClick={this.handleSubmit} />
         </div> 
       </div>
