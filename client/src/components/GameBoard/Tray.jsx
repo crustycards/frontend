b9df46e350e5dcc8b2106e2e8f8426d1f7e48a57
Thomas Volk <tvolk131@gmail.com>
2017-12-17 16:59:28 -0600

    Game board tray shows cards correctly

--- client/src/components/GameBoard/Tray.jsx
+++ client/src/components/GameBoard/Tray.jsx
@@ -3,25 +3,17 @@ import { connect } from 'react-redux';
 import { GridList } from 'material-ui/GridList';
 import { bindActionCreators } from 'redux';
 import Card from '../CAHCard.jsx';
-const style = {
-  width: '100%'
-};
+import { playCard } from '../../gameServerInterface';
 
 const Tray = ({hand}) => (
-  <div style={style}>
-    <GridList
-      cols={hand.length}
-      cellHeight={200}
-      padding={1}
-    >
-      {hand.map(card =>
-        <Card
-          key={card.id} 
-          card={card} 
-          playHandler={() => console.log(card)} 
-        />
-      )}
-    </GridList>
+  <div className='tray'>
+    {hand.map(card =>
+      <Card
+        key={card.id}
+        card={card}
+        playHandler={() => playCard(card.id)}
+      />
+    )}
   </div>
 );
 
