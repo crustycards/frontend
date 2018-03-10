2fda0adee6a00068bf32966e0290fa2a57d4c4d8
Thomas Volk <tvolk131@gmail.com>
2018-03-10 09:24:46 -0600

    Removed database

--- database/connection.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-const config = require('./configLoader.js');
-let connection = new Sequelize(config.database, config.username, config.password, config);
-connection.clear = () => {
-  return connection.sync({force: true});
-};
-
-module.exports = connection;
