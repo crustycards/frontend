8583c05eea17f30333573ba227632bc58deca171
Thomas Volk <tvolk131@gmail.com>
2018-03-27 19:20:24 -0500

    Added basic api support

---
+++ api/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  User: require('./models/user'),
+  Friend: require('./models/friend')
+};
---
+++ api/models/friend.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const api = process.env.API_URL;
+
+const getFriends = async (userId) => {
+  const response = await axios.get(`${api}/user/${userId}/friends`);
+  return response.data;
+}
+
+const getSentRequests = async (userId) => {
+  const response = await axios.get(`${api}/user/${userId}/friends/requests/sent`);
+  return response.data;
+}
+
+const getReceivedRequests = async (userId) => {
+  const response = await axios.get(`${api}/user/${userId}/friends/requests/received`);
+  return response.data;
+}
+
+module.exports = {
+  getFriends,
+  getSentRequests,
+  getReceivedRequests
+};
