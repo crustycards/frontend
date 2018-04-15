ff1616216e8f0087d4f51c720ec689b528366062
Thomas Volk <tvolk131@gmail.com>
2018-04-14 20:57:53 -0500

    Removed cardpack redux module

--- client/src/store/modules/index.js
+++ client/src/store/modules/index.js
@@ -4,12 +4,10 @@ import global from './global.js';
 import home from './home.js';
 import game from './game.js';
 import games from './games.js';
-import cardpacks from './cardpacks.js';
 
 export default combineReducers({
   global,
   home,
   game,
-  games,
-  cardpacks
+  games
 });
