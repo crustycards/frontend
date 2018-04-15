37c5854a09f377e4138131d5032b8ed8780875ab
Thomas Volk <tvolk131@gmail.com>
2018-04-15 11:12:25 -0500

    Renamed redux state reference

--- client/src/apiInterface.js
+++ client/src/apiInterface.js
@@ -3,7 +3,7 @@ import store from './store';
 
 const { apiURL } = window.__PRELOADED_DATA__;
 
-const user = store.getState().global.currentUser;
+const user = store.getState().user.currentUser;
 
 const api = axios.create({
   baseURL: apiURL,
