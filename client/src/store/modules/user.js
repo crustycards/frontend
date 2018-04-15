035e9bfefb53367fc1b724aea61abf005ae86f35
Thomas Volk <tvolk131@gmail.com>
2018-04-14 23:21:26 -0500

    Merged redux home module with user

--- client/src/store/modules/user.js
+++ client/src/store/modules/user.js
@@ -1,12 +1,20 @@
 export const SET_CURRENT_USER = 'user/SET_CURRENT_USER';
 export const ADD_CARDPACK = 'user/ADD_CARDPACK';
 export const REMOVE_CARDPACK = 'user/REMOVE_CARDPACK';
+export const ADD_FRIEND = 'user/ADD_FRIEND';
+export const REMOVE_FRIEND = 'user/REMOVE_FRIEND';
+export const ADD_SENT_FRIEND_REQUEST = 'user/ADD_SENT_FRIEND_REQUEST';
+export const ADD_RECEIVED_FRIEND_REQUEST = 'user/ADD_RECEIVED_FRIEND_REQUEST';
+export const SET_FRIENDS = 'user/SET_FRIENDS';
 
 const preloadedState = window.__PRELOADED_STATE__;
 
 const initialState = {
   currentUser: preloadedState.currentUser,
-  cardpacks: preloadedState.cardpacks
+  cardpacks: preloadedState.cardpacks,
+  friends: preloadedState.friends,
+  requestsSent: preloadedState.requestsSent,
+  requestsReceived: preloadedState.requestsReceived
 };
 
 export default (state = initialState, {type, payload}) => {
@@ -25,6 +33,39 @@ export default (state = initialState, {type, payload}) => {
     return {
       ...state,
       cardpacks: state.cardpacks.filter(cardpack => cardpack.id !== payload)
+    };
+  case ADD_FRIEND: 
+    return {
+      ...state,
+      friends: state.friends.concat([payload]),
+      requestsSent: state.requestsSent.filter(f => f.id !== payload.id),
+      requestsReceived: state.requestsReceived.filter(f => f.id !== payload.id)
+    };
+
+  case SET_FRIENDS: 
+    return {
+      ...state,
+      friends: payload
+    };
+
+  case ADD_SENT_FRIEND_REQUEST:
+    return {
+      ...state,
+      requestsSent: state.requestsSent.concat(payload)
+    };
+
+  case ADD_RECEIVED_FRIEND_REQUEST:
+    return {
+      ...state,
+      requestsReceived: state.requestsReceived.concat(payload)
+    };
+
+  case REMOVE_FRIEND:
+    return {
+      ...state,
+      friends: state.friends.filter(f => f.id !== payload.id),
+      requestsSent: state.requestsSent.filter(f => f.id !== payload.id),
+      requestsReceived: state.requestsReceived.filter(f => f.id !== payload.id)
     };
   
   default: 
@@ -52,3 +93,29 @@ export const removeCardpack = payload => {
     payload
   };
 };
+
+export const addFriend = payload => {
+  return {
+    type: ADD_FRIEND,
+    payload
+  };
+};
+
+export const addFriendRequestSent = payload => {
+  return {
+    type: ADD_SENT_FRIEND_REQUEST,
+    payload
+  };
+};
+
+export const addFriendRequestReceived = payload => {
+  return {
+    type: ADD_RECEIVED_FRIEND_REQUEST,
+    payload
+  };
+};
+
+export const removeFriend = payload => ({
+  type: REMOVE_FRIEND,
+  payload
+});
