2345673f957482d61b169a84e9d284f72b931ae5
Thomas Volk <tvolk131@gmail.com>
2018-04-15 11:12:08 -0500

    Removed reference to home module

--- client/src/store/modules/index.js
+++ client/src/store/modules/index.js
@@ -2,14 +2,12 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import global from './global.js';
 import user from './user.js';
-import home from './home.js';
 import game from './game.js';
 import games from './games.js';
 
 export default combineReducers({
   global,
   user,
-  home,
   game,
   games
 });
