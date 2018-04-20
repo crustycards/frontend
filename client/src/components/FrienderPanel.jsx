e22f2ef0a17d5cf510ac5f6a7455dba055c60949
Thomas Volk <tvolk131@gmail.com>
2018-04-19 19:31:52 -0500

    Added search support to friender component

--- client/src/components/FrienderPanel.jsx
+++ client/src/components/FrienderPanel.jsx
@@ -1,58 +1,75 @@
 import React, { Component } from 'react';
-import api from '../apiInterface';
+import { searchUsers, autocompleteUserSearch } from '../apiInterface';
 import { connect } from 'react-redux';
-import { TextField, RaisedButton } from 'material-ui';
-import helpers from '../helpers';
+import { AutoComplete, RaisedButton } from 'material-ui';
+import { AccessAlarm } from 'material-ui-icons';
+import UserCard from './shells/UserCard.jsx';
 
 class FrienderPanel extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      requestEmail: ''
+      searchQuery: '',
+      autocompleteOptions: [],
+      hasSearched: false,
+      searchedUsers: []
     };
-    this.sendRequest = this.sendRequest.bind(this);
+    this.searchUsers = this.searchUsers.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
-  sendRequest () {
-    if (helpers.isEmail(this.state.requestEmail)) {
-      // TODO - Fix this to search users rather than add by email
-      api.addFriend(this.state.requestEmail);
-      this.setState({requestEmail: ''});
-    }
+  searchUsers() {
+    searchUsers(this.state.searchQuery)
+      .then(searchedUsers => this.setState({searchedUsers, hasSearched: true}));
   }
 
-  handleInputChange (property, e) {
-    let stateChange = {};
-    stateChange[property] = e.target.value;
-    this.setState(stateChange);
+  handleInputChange (searchQuery) {
+    this.setState({searchQuery});
+    autocompleteUserSearch(searchQuery).then(autocompleteOptions => this.setState({autocompleteOptions}));
   }
 
   handleKeyPress (e) {
     if (e.key === 'Enter') {
-      this.sendRequest();
+      this.searchUsers();
     }
   }
 
   render () {
     return (
-      <div className="panel">
-        <div>Add Friends</div>
-        <TextField 
-          onKeyPress={this.handleKeyPress} 
-          hintText='hello@world.com' 
-          floatingLabelText='Email' 
-          type='email' 
-          value={this.state.requestEmail} 
-          onChange={this.handleInputChange.bind(this, 'requestEmail')} 
+      <div className='panel'>
+        <div>Search Users</div>
+        <AutoComplete
+          onKeyPress={this.handleKeyPress}
+          onNewRequest={this.searchUsers}
+          menuCloseDelay={0}
+          hintText='User Name'
+          floatingLabelText='Name'
+          type='email'
+          searchText={this.state.searchQuery}
+          dataSource={this.state.autocompleteOptions}
+          onUpdateInput={this.handleInputChange}
+          filter={AutoComplete.caseInsensitiveFilter}
         />
-        <br/>
-        <RaisedButton 
-          label='Send Friend Request' 
-          onClick={this.sendRequest} 
-          disabled={!helpers.isEmail(this.state.requestEmail)} 
+        <RaisedButton
+          style={{margin: '3px'}}
+          label='Search Users'
+          onClick={this.searchUsers}
+          primary={true}
         />
+        {
+          this.state.hasSearched &&
+          <div style={{marginTop: '14px'}}>
+            {
+              this.state.searchedUsers.length ?
+                this.state.searchedUsers.map((user, index) =>
+                  <UserCard user={user} showFriendButton key={index} />
+                )
+                :
+                <div>No users were found</div>
+            }
+          </div>
+        }
       </div>
     );
   }
