cc19368b1acb786acb0840815460611a9170b08b
Thomas Volk <tvolk131@gmail.com>
2018-05-09 00:57:44 -0500

    Removed createdAt option

--- client/src/components/CAHCard.jsx
+++ client/src/components/CAHCard.jsx
@@ -1,44 +1,19 @@
 import React, {Component} from 'react';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles';
 import { FlatButton } from 'material-ui';
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
-import time from 'time-converter';
 import api from '../apiInterface';
 
 class CAHCard extends Component {
   constructor (props) {
     super(props);
-    this.state = {};
-    if (props.showTime) {
-      this.state.time = this.getTime();
-    }
 
     this.removeCard = this.removeCard.bind(this);
+
     // If we have no playhandler register console.error to 
     // report the error if invoked
     this.playHandler = (this.props.playHandler || console.error).bind(this);
-  }
-
-  componentDidMount() {
-    if (this.props.showTime) {
-      this.intervalId = setInterval(() => {
-        let time = this.getTime();
-        if (time !== this.state.time) {
-          this.setState({time});
-        }
-      }, 100);
-    }
-  }
-
-  componentWillUnmount() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-  }
-
-  getTime() {
-    return 'Created at ' + time.stringify(this.props.card.createdAt, {relativeTime: true});
   }
 
   removeCard() {
@@ -46,35 +21,18 @@ class CAHCard extends Component {
   }
 
   render() {
-    let cardElements = [];
-    cardElements.push(
-      <CardHeader
-        title={this.props.card.text + (this.props.card.answerFields && this.props.card.answerFields > 1 ? ' - ' + this.props.card.answerFields + ' card answer' : '')}
-        subtitle={this.props.showTime ? this.state.time : ''}
-        key={0}
-      />
-    );
-
-    cardElements.push(
-      <CardActions key={1}>
-        {this.props.isOwner ? <FlatButton label='Delete' onClick={this.removeCard} /> : null}
-        {this.props.playHandler ? <FlatButton label='Play' onClick={() => this.playHandler(this.props.card)} /> : null}
-      </CardActions>
-    );
-
-    let cardWrapper = this.props.card.type === 'black' ? (
-      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-        <Card className='card'>;
-        {cardElements}
-        </Card>
-      </MuiThemeProvider>
-    ) : (
-      <Card className='card'>;
-      {cardElements}
+    return <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+      <Card className='card'>
+        <CardHeader
+          title={this.props.card.text + (this.props.card.answerFields && this.props.card.answerFields > 1 ? ' - ' + this.props.card.answerFields + ' card answer' : '')}
+          key={0}
+        />
+        <CardActions key={1}>
+          {this.props.isOwner ? <FlatButton label='Delete' onClick={this.removeCard} /> : null}
+          {this.props.playHandler ? <FlatButton label='Play' onClick={() => this.playHandler(this.props.card)} /> : null}
+        </CardActions>
       </Card>
-    );
-
-    return cardWrapper;
+    </MuiThemeProvider>;
   }
 }
 
