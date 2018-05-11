85015ac6a5470b0d9b1e4eb3bb026b73e37e72b3
Thomas Volk <tvolk131@gmail.com>
2018-05-11 01:05:47 -0500

    Added current black card component

---
+++ client/src/components/GameBoard/CurrentBlackCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import CAHBlackCard from '../shells/CAHBlackCard.jsx';
+import { connect } from 'react-redux';
+
+const parseCardText = (blackCardText, whiteCardTextList) => {
+  let tempText = blackCardText;
+
+  const replacementIndicator = '_____';
+  for (let i = 0; i < whiteCardTextList.length && tempText.includes(replacementIndicator); i++) {
+    tempText = tempText.replace(replacementIndicator, whiteCardTextList[i]);
+  }
+
+  return tempText;
+};
+
+const CurrentBlackCard = ({card, hand, queuedCardIds}) => (
+  card ?
+    <CAHBlackCard card={{...card, text: parseCardText(card.text, hand.map(card => card.text).filter(text => queuedCardIds.includes(text)))}} />
+    :
+    <div>No Black Card</div>
+);
+
+const mapStateToProps = ({game}) => ({
+  card: game.currentBlackCard,
+  hand: game.hand,
+  queuedCardIds: game.queuedCardIds
+});
+
+export default connect(mapStateToProps)(CurrentBlackCard);
