315ef5f3535863784eb38ab1afb353c97e4d88f5
Thomas Volk <tvolk131@gmail.com>
2018-05-19 18:51:42 -0500

    Can refresh game list

--- client/src/components/GameList/List.jsx
+++ client/src/components/GameList/List.jsx
@@ -1,32 +1,65 @@
 import React, { Component } from 'react';
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
-import { FlatButton } from 'material-ui';
+import { RaisedButton, FlatButton, CircularProgress } from 'material-ui';
 import { connect } from 'react-redux';
-import { joinGame, leaveGame } from '../../gameServerInterface';
+import { joinGame, leaveGame, getGameList } from '../../gameServerInterface';
+import { setTimeout } from 'timers';
 
-const GameList = (props) => (
-  props.games.length ?
-    <div>
-      <h2>Join Game</h2>
-      {props.games.map((game, index) => (
-        <Card key={index} className='card'>
-          <CardHeader
-            title={game.name}
-            subtitle={`Host: ${game.owner.name} (${game.owner.email})`}
-          />
-          <CardActions>
-            {
-              props.game && props.game.name === game.name ?
-                <FlatButton label='Leave' onClick={leaveGame} /> :
-                <FlatButton label='Join' onClick={() => { joinGame(game.name); }} />
-            }
-          </CardActions>
-        </Card>
-      ))}
-    </div>
-    :
-    <div className='center'><span>There are no open games to join</span></div>
-);
+class GameList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true
+    };
+
+    this.refresh = this.refresh.bind(this);
+  }
+
+  componentDidMount() {
+    this.refresh();
+  }
+
+  refresh() {
+    this.setState({isLoading: true});
+    getGameList().then(() => this.setState({isLoading: false}));
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>
+        <h2>Games</h2>
+        <div className='center'><CircularProgress size={80} thickness={5} /></div>
+      </div>;
+    }
+
+    if (!this.props.games.length) {
+      return <div className='center'><span>There are no open games to join</span></div>;
+    }
+
+    return (
+      <div>
+        <h2>Games</h2>
+        <RaisedButton label='Refresh' onClick={this.refresh} />
+        {this.props.games.map((game, index) => (
+          <Card style={this.props.game && this.props.game.name === game.name ? {filter: 'brightness(90%)'} : {}} key={index} className='card'>
+            <CardHeader
+              title={game.name}
+              subtitle={`Host: ${game.owner.name} (${game.owner.email})`}
+            />
+            <CardActions>
+              {
+                this.props.game && this.props.game.name === game.name ?
+                  <FlatButton label='Leave' onClick={() => { leaveGame().then(this.refresh); }} /> :
+                  <FlatButton label='Join' onClick={() => { joinGame(game.name); }} />
+              }
+            </CardActions>
+          </Card>
+        ))}
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = ({game, games}) => ({
   game,
