abef6ebca2b0f5f94fadbd5db7a5de8ed7b46245
Thomas Volk <tvolk131@gmail.com>
2018-05-20 16:26:17 -0500

    Changed cah card components to functional

--- client/src/components/shells/CAHBlackCard.jsx
+++ client/src/components/shells/CAHBlackCard.jsx
@@ -5,35 +5,27 @@ import { FlatButton } from 'material-ui';
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 import { deleteBlackCard } from '../../apiInterface';
 
-class CAHBlackCard extends Component {
-  constructor (props) {
-    super(props);
-
-    this.removeCard = this.removeCard.bind(this);
-  }
-
-  removeCard() {
-    return deleteBlackCard(this.props.card.id).then((data) => {
-      if (this.props.onDelete) {
-        this.props.onDelete(this.props.card.id);
+const CAHBlackCard = (props) => {
+  const removeCard = () => {
+    deleteBlackCard(props.card.id).then((data) => {
+      if (props.onDelete) {
+        props.onDelete(props.card.id);
       }
       return data;
     });
-  }
+  };
 
-  render() {
-    return <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-      <Card className='card'>
-        <CardHeader
-          title={this.props.card.text}
-          subtitle={this.props.card.answerFields}
-        />
-        <CardActions>
-          {this.props.isOwner ? <FlatButton label='Delete' onClick={this.removeCard} /> : null}
-        </CardActions>
-      </Card>
-    </MuiThemeProvider>;
-  }
-}
+  return <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <Card className='card'>
+      <CardHeader
+        title={props.card.text}
+        subtitle={props.card.answerFields}
+      />
+      <CardActions>
+        {props.isOwner ? <FlatButton label='Delete' onClick={removeCard} /> : null}
+      </CardActions>
+    </Card>
+  </MuiThemeProvider>;
+};
 
 module.exports = CAHBlackCard;
--- client/src/components/shells/CAHWhiteCard.jsx
+++ client/src/components/shells/CAHWhiteCard.jsx
@@ -5,34 +5,26 @@ import { FlatButton } from 'material-ui';
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 import { deleteWhiteCard } from '../../apiInterface';
 
-class CAHWhiteCard extends Component {
-  constructor (props) {
-    super(props);
-
-    this.removeCard = this.removeCard.bind(this);
-  }
-
-  removeCard() {
-    return deleteWhiteCard(this.props.card.id).then((data) => {
-      if (this.props.onDelete) {
-        this.props.onDelete(this.props.card.id);
+const CAHWhiteCard = (props) => {
+  const removeCard = () => {
+    return deleteWhiteCard(props.card.id).then((data) => {
+      if (props.onDelete) {
+        props.onDelete(props.card.id);
       }
       return data;
     });
-  }
+  };
 
-  render() {
-    return <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-      <Card className='card'>
-        <CardHeader
-          title={this.props.card.text}
-        />
-        <CardActions>
-          {this.props.isOwner ? <FlatButton label='Delete' onClick={this.removeCard} /> : null}
-        </CardActions>
-      </Card>
-    </MuiThemeProvider>;
-  }
-}
+  return <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+    <Card className='card'>
+      <CardHeader
+        title={props.card.text}
+      />
+      <CardActions>
+        {props.isOwner ? <FlatButton label='Delete' onClick={removeCard} /> : null}
+      </CardActions>
+    </Card>
+  </MuiThemeProvider>;
+};
 
 module.exports = CAHWhiteCard;
