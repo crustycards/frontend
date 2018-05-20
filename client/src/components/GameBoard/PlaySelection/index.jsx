e912c2506126d857127b104a86dd8731a541ed86
Thomas Volk <tvolk131@gmail.com>
2018-05-20 13:49:29 -0500

    Can play cards if user is not the judge

--- client/src/components/GameBoard/PlaySelection/index.jsx
+++ client/src/components/GameBoard/PlaySelection/index.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Tray from './Tray.jsx';
 import PlayArea from './PlayArea.jsx';
+import { RaisedButton } from 'material-ui';
+import { playCards } from '../../../gameServerInterface';
 
-const PlaySelection = () => (<div><Tray/><PlayArea/></div>);
+const PlaySelection = (props) => (
+  props.currentUserId === props.currentJudgeId ?
+    <div>
+      <Tray/>
+    </div>
+    :
+    <div>
+      <Tray/>
+      <PlayArea/>
+      <RaisedButton
+        label={'Play'}
+        onClick={() => { playCards(props.queuedCardIds); }}
+      />
+    </div>
+);
 
-export default PlaySelection;
+const mapStateToProps = ({game, user: {currentUser}}) => ({
+  currentUserId: currentUser.id,
+  currentJudgeId: game.judgeId,
+  queuedCardIds: game.queuedCardIds
+});
+
+export default connect(mapStateToProps)(PlaySelection);
