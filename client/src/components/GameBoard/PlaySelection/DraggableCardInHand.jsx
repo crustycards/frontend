bca95c9abf065a7af9754e6cd85b20cfa5facc5b
Thomas Volk <tvolk131@gmail.com>
2018-05-19 22:29:13 -0500

    Refactored draggable cards to be functional components

--- client/src/components/GameBoard/PlaySelection/DraggableCardInHand.jsx
+++ client/src/components/GameBoard/PlaySelection/DraggableCardInHand.jsx
@@ -3,14 +3,9 @@ import DragSource from 'react-dnd/lib/DragSource';
 import CAHWhiteCard from '../../shells/CAHWhiteCard.jsx';
 import { cardInPlayQueue } from '../../../dndTypes';
 
-class DraggableCard extends Component {
-  render() {
-    const { isDragging, connectDragSource } = this.props;
-    const opacity = isDragging ? 0.5 : 1;
-
-    return connectDragSource(<div style={{ opacity }}><CAHWhiteCard {...this.props} /></div>);
-  }
-}
+const DraggableCard = (props) => (
+  props.connectDragSource(<div style={{ opacity: props.isDragging ? 0.5 : 1 }}><CAHWhiteCard {...props} /></div>)
+);
 
 const endDrag = (props, monitor) => {
   if (monitor.didDrop()) {
