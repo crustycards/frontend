793e3a85e83b497e0a27b163dbd634b915cd5dd8
Thomas Volk <tvolk131@gmail.com>
2018-06-02 12:06:51 -0500

    Removed old cardpack parsing code

--- client/src/helpers/cardpackFileHandler.js
+++ client/src/helpers/cardpackFileHandler.js
@@ -16,47 +16,6 @@ module.exports.parse = (string) => {
   });
 
   return { whiteCards, blackCards };
-
-
-
-
-
-
-
-
-
-
-
-  // let cards = [];
-  // let lines = string.split(/\r\n\r\n/);
-  // lines.forEach((row, i) => {
-  //   let cardParts = row.split(/\r\n/);
-  //   let cardText = cardParts[0];
-  //   cardText = cardText ? cardText.trim() : null;
-  //   let cardType = cardParts[1];
-  //   cardType = cardType ? cardType.trim() : null;
-  //   if (cardType === 'black') {
-  //     if (!(cardParts.length === 2 || cardParts.length === 3)) {
-  //       throw new Error('Expected either two or three parameters on line ' + (i + 1) + ' but got ' + cardParts.length);
-  //     }
-  //   } else if (cardParts.length !== 2) {
-  //     throw new Error('Expected two parameters on line ' + (i + 1) + ' but got ' + cardParts.length);
-  //   }
-  //   let card = {text: cardText, type: cardType};
-  //   if (card.type === 'black') {
-  //     let answerFields = cardParts[2] || '1';
-  //     answerFields = answerFields.trim();
-  //     answerFields = parseInt(answerFields);
-  //     card.answerFields = answerFields;
-  //   } else {
-  //     card.answerFields = null;
-  //   }
-  //   if (!(card.type === 'black' || card.type === 'white')) {
-  //     throw new Error('Line ' + (i + 1) + ': card type must be either black or white, not ' + card.type);
-  //   }
-  //   cards.push(card);
-  // });
-  // return cards;
 };
 
 module.exports.stringify = ({whiteCards, blackCards}) => {
