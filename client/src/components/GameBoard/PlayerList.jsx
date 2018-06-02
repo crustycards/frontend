9da86c5f5a838bb2846111b23dc2f861680f64b5
Thomas Volk <tvolk131@gmail.com>
2018-06-02 16:36:10 -0500

    Shows whether users have played in player list

--- client/src/components/GameBoard/PlayerList.jsx
+++ client/src/components/GameBoard/PlayerList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { List, ListItem } from 'material-ui/List';
-import { Star } from 'material-ui-icons';
+import { Star, Check } from 'material-ui-icons';
 
 const styles = {
   overflowY: 'auto',
@@ -11,22 +11,22 @@ const styles = {
 
 // TODO - Render game owner uniquely
 
-let renderPlayer = (player, index, ownerId, judgeId) => {
+let renderPlayer = (player, index, ownerId, judgeId, hasPlayed) => {
   if (player.id === ownerId && player.id === judgeId) {
-    return <ListItem leftIcon={<Star/>} primaryText={player.name} secondaryText={`Score: ${player.score}`}/>;
-  } else if (player.id === ownerId) {
-    return <ListItem primaryText={player.name} secondaryText={`Score: ${player.score}`}/>;
+    return <ListItem rightIcon={<Star/>} primaryText={player.name} secondaryText={`Score: ${player.score}`}/>;
   } else if (player.id === judgeId) {
-    return <ListItem leftIcon={<Star/>} primaryText={player.name} secondaryText={`Score: ${player.score}`}/>;
+    return <ListItem rightIcon={<Star/>} primaryText={player.name} secondaryText={`Score: ${player.score}`}/>;
+  } else if (player.id === ownerId) {
+    return <ListItem rightIcon={hasPlayed && <Check/>} primaryText={player.name} secondaryText={`Score: ${player.score}`}/>;
   } else {
-    return <ListItem primaryText={player.name} secondaryText={`Score: ${player.score}`}/>;
+    return <ListItem rightIcon={hasPlayed && <Check/>} primaryText={player.name} secondaryText={`Score: ${player.score}`}/>;
   }
 };
 
-const PlayerList = ({players, ownerId, judgeId}) => (
+const PlayerList = ({players, ownerId, judgeId, whitePlayed, currentBlackCard}) => (
   <List style={styles}>
     {players.map((player, index) => {
-      return <div key={index}>{renderPlayer(player, index, ownerId, judgeId)}</div>;
+      return <div key={index}>{renderPlayer(player, index, ownerId, judgeId, whitePlayed && whitePlayed[player.id] && whitePlayed[player.id].length === currentBlackCard.answerFields)}</div>;
     })}
   </List>
 );
@@ -34,7 +34,9 @@ const PlayerList = ({players, ownerId, judgeId}) => (
 const mapStateToProps = ({game}) => ({
   players: game.players,
   ownerId: game.ownerId,
-  judgeId: game.judgeId
+  judgeId: game.judgeId,
+  whitePlayed: game.whitePlayed,
+  currentBlackCard: game.currentBlackCard
 });
 
 export default connect(mapStateToProps)(PlayerList);
