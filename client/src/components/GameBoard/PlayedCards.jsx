fc1e00caf15c2b6b3701887dcfc75eff5b0f3b4b
Thomas Volk <tvolk131@gmail.com>
2018-06-02 16:35:58 -0500

    Shows whether users have played in the player list

--- client/src/components/GameBoard/PlayedCards.jsx
+++ client/src/components/GameBoard/PlayedCards.jsx
@@ -5,12 +5,20 @@ import { vote } from '../../gameServerInterface';
 import { RaisedButton } from 'material-ui';
 
 const PlayedCards = (props) => {
-  return (
-    <div className={'panel'}>
-      {props.game.whitePlayedAnonymous && props.game.whitePlayedAnonymous.map((cards, index) => <div className={'subpanel'} key={index}>{cards.map((card, index) => <WhiteCard card={card} key={index} />)}{props.currentUser.id === props.game.judgeId && <RaisedButton label={'Vote'} onClick={() => { vote(cards[0].id); }} />}</div>)}
-      {!props.game.whitePlayedAnonymous && Object.keys(props.game.whitePlayed).map((userId, index) => <div className={'subpanel'} key={index}><b>{props.game.players.find(player => player.id === userId).name}</b>{props.game.whitePlayed[userId].map((f, index) => <WhiteCard card={{text: '_____'}} key={index} />)}</div>)}
-    </div>
-  );
+  if (!props.game.whitePlayedAnonymous) {
+    return <div></div>;
+  } else {
+    return (
+      <div className={'panel'}>
+        {props.game.whitePlayedAnonymous.map((cards, index) => (
+          <div className={'subpanel'} key={index}>
+            {cards.map((card, index) => <WhiteCard card={card} key={index} />)}
+            {props.currentUser.id === props.game.judgeId && <RaisedButton label={'Vote'} onClick={() => { vote(cards[0].id); }} />}
+          </div>
+        ))}
+      </div>
+    );
+  }
 };
 
 const mapStateToProps = ({game, user: {currentUser}}) => ({game, currentUser});
