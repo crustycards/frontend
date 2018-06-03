60157b758c1009abc1075b016b13846b5798adf9
Thomas Volk <tvolk131@gmail.com>
2018-06-02 19:25:51 -0500

    Can click card set before voting

--- client/src/components/GameBoard/PlayedCards.jsx
+++ client/src/components/GameBoard/PlayedCards.jsx
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import WhiteCard from '../shells/CAHWhiteCard.jsx';
 import { vote } from '../../gameServerInterface';
 import { RaisedButton } from 'material-ui';
 
-const PlayedCards = (props) => {
-  if (!props.game.whitePlayedAnonymous) {
-    return <div></div>;
-  } else {
-    return (
-      <div className={'panel'}>
-        {props.game.whitePlayedAnonymous.map((cards, index) => (
-          <div className={'subpanel'} key={index}>
-            {cards.map((card, index) => <WhiteCard card={card} key={index} />)}
-            {props.currentUser.id === props.game.judgeId && <RaisedButton label={'Vote'} onClick={() => { vote(cards[0].id); }} />}
-          </div>
-        ))}
-      </div>
-    );
+class PlayedCards extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedSetIndex: null
+    };
   }
-};
+
+  render() {
+    if (!this.props.game.whitePlayedAnonymous) {
+      return null;
+    } else {
+      return (
+        <div className={'panel'}>
+          {this.props.currentUser.id === this.props.game.judgeId && <RaisedButton label={'Vote'} disabled={this.state.selectedSetIndex === null} onClick={() => { vote(this.props.game.whitePlayedAnonymous[this.state.selectedSetIndex][0].id); }} />}
+          {this.props.game.whitePlayedAnonymous.map((cards, index) => (
+            <div
+              className={'subpanel'}
+              key={index}
+              onClick={() => {
+                if (this.props.currentUser.id === this.props.game.judgeId) {
+                  this.setState({selectedSetIndex: index});
+                }
+              }}
+              style={index === this.state.selectedSetIndex ? {background: 'green', transition: 'background .25s ease'} : {transition: 'background .2s ease'}}
+            >
+              {cards.map((card, index) => <WhiteCard card={card} key={index} />)}
+            </div>
+          ))}
+        </div>
+      );
+    }
+  }
+}
 
 const mapStateToProps = ({game, user: {currentUser}}) => ({game, currentUser});
 
