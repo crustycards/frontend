cd8147fbeecd0f8a01fd776aa3f5901051680fac
Tommy Volk <tvolk131@gmail.com>
2018-06-05 18:02:27 -0500

    Can click cards to queue and unqueue

--- client/src/components/GameBoard/PlaySelection/DraggableCardInHand.jsx
+++ client/src/components/GameBoard/PlaySelection/DraggableCardInHand.jsx
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import DragSource from 'react-dnd/lib/DragSource';
 import CAHWhiteCard from '../../shells/CAHWhiteCard.jsx';
 import { cardInPlayQueue } from '../../../dndTypes';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { queueCard } from '../../../store/modules/game';
 
 class DraggableCard extends Component {
   render() {
-    return this.props.connectDragSource(<div style={{ opacity: this.props.isDragging ? 0.5 : 1 }}><CAHWhiteCard {...this.props} /></div>);
+    return this.props.connectDragSource(<div onClick={() => this.props.queueCard(this.props.card.id)} style={{ opacity: this.props.isDragging ? 0.5 : 1 }}><CAHWhiteCard {...this.props} /></div>);
   }
 }
 
@@ -15,7 +18,13 @@ const endDrag = (props, monitor) => {
   }
 };
 
-export default DragSource(cardInPlayQueue, {beginDrag: () => ({}), endDrag}, (connect, monitor) => ({
+const DragSourceCard = DragSource(cardInPlayQueue, {beginDrag: () => ({}), endDrag}, (connect, monitor) => ({
   connectDragSource: connect.dragSource(),
   isDragging: monitor.isDragging(),
 }))(DraggableCard);
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  queueCard
+}, dispatch);
+
+export default connect(null, mapDispatchToProps)(DragSourceCard);
--- client/src/components/GameBoard/PlaySelection/DraggableCardInPlayQueue.jsx
+++ client/src/components/GameBoard/PlaySelection/DraggableCardInPlayQueue.jsx
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import DragSource from 'react-dnd/lib/DragSource';
 import CAHWhiteCard from '../../shells/CAHWhiteCard.jsx';
 import { cardInHand } from '../../../dndTypes';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { unqueueCard } from '../../../store/modules/game';
 
 class DraggableCard extends Component {
   render() {
-    return this.props.connectDragSource(<div style={{ opacity: this.props.isDragging ? 0.5 : 1 }}><CAHWhiteCard {...this.props} /></div>);
+    return this.props.connectDragSource(<div onClick={() => this.props.unqueueCard(this.props.card.id)} style={{ opacity: this.props.isDragging ? 0.5 : 1 }}><CAHWhiteCard {...this.props} /></div>);
   }
 }
 
@@ -15,7 +18,13 @@ const endDrag = (props, monitor) => {
   }
 };
 
-export default DragSource(cardInHand, {beginDrag: () => ({}), endDrag}, (connect, monitor) => ({
+const DragSourceCard = DragSource(cardInHand, {beginDrag: () => ({}), endDrag}, (connect, monitor) => ({
   connectDragSource: connect.dragSource(),
   isDragging: monitor.isDragging(),
 }))(DraggableCard);
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  unqueueCard
+}, dispatch);
+
+export default connect(null, mapDispatchToProps)(DragSourceCard);
