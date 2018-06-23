5788f3bb1cf8b2275cf4943f99c12cd32c660da4
Tommy Volk <tvolk131@gmail.com>
2018-06-22 21:17:12 -0500

    Autofill is awesomer

--- client/src/components/GameBoard/CurrentBlackCard.jsx
+++ client/src/components/GameBoard/CurrentBlackCard.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
-import CAHBlackCard from '../shells/CAHBlackCard.jsx';
-import { connect } from 'react-redux';
+import React from 'react'
+import CAHBlackCard from '../shells/CAHBlackCard.jsx'
+import { Typography } from '@material-ui/core'
+import { connect } from 'react-redux'
 
 const parseCardText = (blackCardText, whiteCardTextList) => {
-  let tempText = blackCardText;
+  let tempText = blackCardText
+  const tempWhiteTexts = [...whiteCardTextList]
 
-  const replacementIndicator = '_____';
-  for (let i = 0; i < whiteCardTextList.length && tempText.includes(replacementIndicator); i++) {
-    tempText = tempText.replace(replacementIndicator, whiteCardTextList[i]);
+  const replacementIndicator = '_____'
+
+  while (true) {
+    const reducedUnderscoreText = tempText.replace(replacementIndicator + '_', replacementIndicator)
+    if (reducedUnderscoreText === tempText) {
+      break
+    } else {
+      tempText = reducedUnderscoreText
+    }
   }
 
-  return tempText;
-};
+  for (const i in tempWhiteTexts) {
+    // TODO - Don't remove period if card ends in multiple ...'s
+    if (tempWhiteTexts[i].endsWith('.')) {
+      tempWhiteTexts[i] = tempWhiteTexts[i].slice(0, tempWhiteTexts[i].length - 1)
+    }
+  }
+
+  const splitBlackText = tempText.split(replacementIndicator)
+  const injectedBlackTextElements = []
+
+  for (let i = 0; i < splitBlackText.length - 1; i++) {
+    injectedBlackTextElements.push(splitBlackText[i])
+    if (tempWhiteTexts[i]) {
+      injectedBlackTextElements.push(<Typography key={i} style={{display: 'inline'}} variant={'title'} color={'secondary'}>{tempWhiteTexts[i]}</Typography>)
+    } else {
+      injectedBlackTextElements.push(replacementIndicator)
+    }
+  }
+  injectedBlackTextElements.push(splitBlackText[splitBlackText.length - 1])
+
+  return injectedBlackTextElements
+}
 
 const CurrentBlackCard = ({card, hand, queuedCardIds}) => (
   card ?
