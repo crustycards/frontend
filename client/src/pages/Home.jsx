1925df6206116b4bc8b76d3f559ac5bbb79431fc
Tommy Volk <tvolk131@gmail.com>
2018-07-07 17:53:12 -0500

    Removed components from home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import FriendsList from '../components/FriendsList.jsx';
-import FriendRequestsSent from '../components/FriendRequestsSent.jsx';
-import FriendRequestsReceived from '../components/FriendRequestsReceived.jsx';
 import FrienderPanel from '../components/FrienderPanel.jsx';
 
 const Home = (props) => (
   <div className='content-wrap'>
     <div className='col-narrow'>
       <FrienderPanel />
-      <FriendsList />
-      <FriendRequestsSent />
-      <FriendRequestsReceived />
     </div>
     <div className='col-wide'>
     </div>
