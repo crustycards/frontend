ba7af6a9f5bfc6df8aad1c1f15f0ada85086084d
Tommy Volk <tvolk131@gmail.com>
2018-07-11 00:37:35 -0500

    Removed userpage redux module

--- client/src/store/modules/index.js
+++ client/src/store/modules/index.js
@@ -2,13 +2,11 @@ import {combineReducers} from 'redux';
 import global from './global.js';
 import game from './game.js';
 import games from './games.js';
-import userPage from './userPage';
 import {reducer as formReducer} from 'redux-form';
 
 export default combineReducers({
   global,
   game,
   games,
-  userPage,
   form: formReducer
 });
