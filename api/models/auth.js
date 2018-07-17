e46e377631976046779e45af868320f0d3624b2a
Tommy Volk <tvolk131@gmail.com>
2018-07-17 17:20:39 -0500

    Added interaction methods for session api

---
+++ api/models/auth.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const auth = process.env.AUTH_SERVER_URL;
+
+const createSession = async (userId) => {
+  const response = await axios.put(`${auth}/session?userId=${userId}`);
+  return response.data;
+};
+
+const getSession = (sessionId) => {
+  return axios.get(`${auth}/session?sessionId=${sessionId}`)
+    .then((res) => res.data)
+    .catch(() => null);
+};
+
+const deleteSession = (sessionId) => {
+  return axios.delete(`${auth}/session?sessionId=${sessionId}`)
+    .then(() => {});
+};
+
+const linkSessionToFirebase = (sessionId, firebaseToken) => {
+  return axios.put(`${auth}/session/${sessionId}?token=${firebaseToken}`)
+    .then(() => {});
+};
+
+module.exports = {
+  createSession,
+  getSession,
+  deleteSession,
+  linkSessionToFirebase
+};
