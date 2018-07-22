ab82c96c2c58eaaab977f5c023aa380781f081f8
Tommy Volk <tvolk131@gmail.com>
2018-07-21 19:35:03 -0500

    Added types for api context

--- client/src/api/context.ts
+++ client/src/api/context.ts
@@ -1,6 +1,20 @@
 import * as React from 'react';
 
-const Context = React.createContext({});
+import ApiInterface from './apiInterface';
+import GameApiInterface from './gameApiInterface';
+
+import HttpApi from './httpApi';
+import HttpGameApi from './httpGameApi';
+
+interface ApiContext {
+  api: ApiInterface
+  gameApi: GameApiInterface
+}
+
+const Context = React.createContext<ApiContext>({
+  api: new HttpApi(''),
+  gameApi: new HttpGameApi('')
+});
 
 export const Provider = Context.Provider;
 export const Consumer = Context.Consumer;
