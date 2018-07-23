04026bfe676f91cc8c37c7519f46ddc3a0b96a2a
Tommy Volk <tvolk131@gmail.com>
2018-07-23 17:23:27 -0500

    Fixed test import

--- test/fullPageRenders/Game/Game.test.js
+++ test/fullPageRenders/Game/Game.test.js
@@ -1,4 +1,4 @@
-import Game from '../../../client/src/pages/Game.jsx';
+import Game from '../../../client/src/pages/Game.tsx';
 import states from './Game.states.json';
 import {generateTests} from '../testGen';
 
