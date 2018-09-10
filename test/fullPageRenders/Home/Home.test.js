4e0d0546bf6cf4c43a5c43427ff2aac619997bdd
Tommy Volk <tvolk131@gmail.com>
2018-09-10 15:47:08 -0500

    Fixed test import issues

--- test/fullPageRenders/Home/Home.test.js
+++ test/fullPageRenders/Home/Home.test.js
@@ -1,4 +1,4 @@
-import Home from '../../../client/src/pages/Home.jsx';
+import Home from '../../../client/src/pages/Home.tsx';
 import states from './Home.states.json';
 import {generateTests} from '../testGen';
 
