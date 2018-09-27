000016c92c0e2ea000f9974dfa30b2c4b070454a
Tommy Volk <tvolk131@gmail.com>
2018-09-27 11:22:03 -0500

    UrlImage can contain onError prop

--- client/src/components/UrlImage.tsx
+++ client/src/components/UrlImage.tsx
@@ -4,6 +4,7 @@ import {Component} from 'react';
 interface UrlImageProps {
   url: string
   onLoad?(e: React.SyntheticEvent<HTMLImageElement>): void
+  onError?(e: React.SyntheticEvent<HTMLImageElement>): void
   loadingView?: React.ReactNode
   style?: React.CSSProperties
   imageStyle?: React.CSSProperties
@@ -22,11 +23,19 @@ class UrlImage extends Component<UrlImageProps, UrlImageState> {
     };
 
     this.handleLoad = this.handleLoad.bind(this);
+    this.handleError = this.handleError.bind(this);
   }
 
   handleLoad(e: React.SyntheticEvent<HTMLImageElement>) {
     if (this.props.onLoad) {
       this.props.onLoad(e);
+    }
+    this.setState({isLoading: false});
+  }
+
+  handleError(e: React.SyntheticEvent<HTMLImageElement>) {
+    if (this.props.onError) {
+      this.props.onError(e);
     }
     this.setState({isLoading: false});
   }
@@ -37,6 +46,7 @@ class UrlImage extends Component<UrlImageProps, UrlImageState> {
         <img
           src={this.props.url}
           onLoad={this.handleLoad}
+          onError={this.handleError}
           style={this.props.imageStyle}
         />
         {
