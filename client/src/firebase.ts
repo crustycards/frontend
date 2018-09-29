710ce7019ed7709d462f914a1435bea7cd18a168
Tommy Volk <tvolk131@gmail.com>
2018-09-29 13:54:34 -0500

    Temporarily disabled firebase

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -5,13 +5,14 @@ const config = {
   messagingSenderId: '203333096571' // TODO - Dynamically insert Firebase sender ID
 };
 
-export const init = (onMessage: (payload: any) => void) => {
-  firebase.initializeApp(config);
-  const messaging = firebase.messaging();
-  onMessage && messaging.onMessage(onMessage);
-  return messaging.requestPermission()
-    .catch(() => console.log('Denied permission for notifications'))
-    .then(() => {
-      return messaging.getToken();
-    });
+export const init = async (onMessage: (payload: any) => void) => {
+  // TODO - Enable Firebase once HTTPS is setup
+  // firebase.initializeApp(config);
+  // const messaging = firebase.messaging();
+  // onMessage && messaging.onMessage(onMessage);
+  // return messaging.requestPermission()
+  //   .catch(() => console.log('Denied permission for notifications'))
+  //   .then(() => {
+  //     return messaging.getToken();
+  //   });
 };
