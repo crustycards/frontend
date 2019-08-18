52b5597dbb31005d062eee14407b52cdb8c8167a
Tommy Volk <tvolk131@gmail.com>
2019-08-18 19:06:13 +0000

    Clean up profile page

--- src/client/src/pages/User.tsx
+++ src/client/src/pages/User.tsx
@@ -1,4 +1,5 @@
-import {CircularProgress, Grid} from '@material-ui/core';
+import {CircularProgress, Fab, Grid, Typography} from '@material-ui/core';
+import EditIcon from '@material-ui/icons/Edit';
 import * as queryString from 'query-string';
 import * as React from 'react';
 import {Component} from 'react';
@@ -8,7 +9,7 @@ import {Cardpack, User as UserModel} from '../api/dao';
 import Api from '../api/model/api';
 import CardpackCreator from '../components/CardpackCreator';
 import CardpackList from '../components/CardpackList/index';
-import ProfileImageUploader from '../components/ProfileImageUploader';
+import ProfileEditorDialog from '../components/ProfileEditorDialog';
 import UrlImage from '../components/UrlImage';
 
 interface UserProps {
@@ -25,6 +26,7 @@ interface UserState {
   successfullyLoaded: boolean;
   user: UserModel;
   cardpacks: Cardpack[];
+  showProfileEditorDialog: boolean;
 }
 
 class User extends Component<UserProps, UserState> {
@@ -33,12 +35,16 @@ class User extends Component<UserProps, UserState> {
 
     const userId = queryString.parse(props.location.search).id;
 
+    this.openProfileEditorDialog = this.openProfileEditorDialog.bind(this);
+    this.closeProfileEditorDialog = this.closeProfileEditorDialog.bind(this);
+
     this.state = {
       isCurrentUser: this.props.user && this.props.user.id === userId,
       isLoading: true,
       successfullyLoaded: false,
       user: undefined,
-      cardpacks: undefined
+      cardpacks: undefined,
+      showProfileEditorDialog: false
     };
 
     if (typeof userId === 'string') {
@@ -77,43 +83,66 @@ class User extends Component<UserProps, UserState> {
     }
 
     return (
-      <Grid container spacing={8}>
-        <Grid item xs={12} md={5}>
-          {this.state.user.name}
-          {
-            this.state.isCurrentUser &&
-            <ProfileImageUploader/>
-          }
-          <UrlImage
-            url={this.props.api.main.getProfileImageUrl(this.state.user.id)}
-            loadingView={<CircularProgress/>}
-            errorView={<div>No profile image</div>}
-            imageStyle={{
-              height: '150px',
-              width: '150px',
-              borderRadius: '5px'
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} md={7}>
-          {
-            this.state.isCurrentUser &&
-            <CardpackCreator onSubmit={(cardpack) => {
-              this.setState({cardpacks: this.state.cardpacks.concat(cardpack)});
-            }} />
-          }
-          <CardpackList
-            cardpacks={this.state.cardpacks}
-            canDelete={this.state.isCurrentUser}
-            onDelete={(id) => {
-              this.setState({
-                cardpacks: this.state.cardpacks.filter((cardpack) => cardpack.id !== id)
-              });
-            }}
-          />
-        </Grid>
-      </Grid>
+      <div className='content-wrap'>
+        <div className='panel'>
+          <Grid container spacing={8}>
+            <Grid item xs={12} md={5}>
+              <UrlImage
+                url={this.props.api.main.getProfileImageUrl(this.state.user.id)}
+                loadingView={<CircularProgress/>}
+                errorView={<div>No profile image</div>}
+                imageStyle={{
+                  height: '150px',
+                  width: '150px',
+                  borderRadius: '5px'
+                }}
+              />
+              <Typography component={'h5'} variant={'h5'}>{this.state.user.name}</Typography>
+              {
+                this.state.isCurrentUser &&
+                <div>
+                  <Fab color={'secondary'} aria-label={'Edit'} onClick={this.openProfileEditorDialog}>
+                    <EditIcon/>
+                  </Fab>
+                  <ProfileEditorDialog
+                    isVisible={this.state.showProfileEditorDialog}
+                    onClose={this.closeProfileEditorDialog}
+                    onUsernameChange={(username) => this.setState({
+                      user: {...this.state.user, name: username}
+                    })}
+                  />
+                </div>
+              }
+            </Grid>
+            <Grid item xs={12} md={7}>
+              {
+                this.state.isCurrentUser &&
+                <CardpackCreator onSubmit={(cardpack) => {
+                  this.setState({cardpacks: this.state.cardpacks.concat(cardpack)});
+                }} />
+              }
+              <CardpackList
+                cardpacks={this.state.cardpacks}
+                canDelete={this.state.isCurrentUser}
+                onDelete={(id) => {
+                  this.setState({
+                    cardpacks: this.state.cardpacks.filter((cardpack) => cardpack.id !== id)
+                  });
+                }}
+              />
+            </Grid>
+          </Grid>
+        </div>
+      </div>
     );
+  }
+
+  private openProfileEditorDialog() {
+    this.setState({showProfileEditorDialog: true});
+  }
+
+  private closeProfileEditorDialog() {
+    this.setState({showProfileEditorDialog: false});
   }
 }
 
