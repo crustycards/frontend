29524f0bd1ddae3f578fabed01edf12b812b995b
Tommy Volk <tvolk131@gmail.com>
2021-05-11 01:10:37 +0000

    Add infrastructure for screenshot testing

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -1,4 +1,6 @@
 import * as Enzyme from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
+import {toMatchImageSnapshot} from 'jest-image-snapshot';
 
+expect.extend({toMatchImageSnapshot});
 Enzyme.configure({adapter: new Adapter()});
