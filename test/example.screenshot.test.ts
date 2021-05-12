df28b52373488aabff4def8b857da0bbce4ee307
Tommy Volk <tvolk131@gmail.com>
2021-05-12 18:32:29 +0000

    Cleanup

--- test/example.screenshot.test.ts
+++ test/example.screenshot.test.ts
@@ -9,7 +9,7 @@ afterEach(() => {
   sinon.restore();
 });
 
-it('navigates to Google', async () => {
+it('shows login page on mobile', async () => {
     const browser = await puppeteer.launch({args: ['--no-sandbox']});
     const page = await browser.newPage();
     await page.setViewport({
@@ -18,24 +18,34 @@ it('navigates to Google', async () => {
       deviceScaleFactor: 1,
     });
     await page.goto('http://localhost:3000/');
+    let image = await page.screenshot();
+    expect(image).toMatchImageSnapshot();
+});
+
+it('shows main page on mobile', async () => {
+    const browser = await puppeteer.launch({args: ['--no-sandbox']});
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 320,
+      height: 568,
+      deviceScaleFactor: 1,
+    });
     await page.setCookie({
       name: 'authToken',
       value: createMockJwt('users/test'),
-      url: 'http://localhost:3000/'
+      domain: 'localhost:3000',
     });
-
+    await page.goto('http://localhost:3000/');
     let image = await page.screenshot();
     expect(image).toMatchImageSnapshot();
-    let stub = sinon.stub();
-    sinon.replace(mockUserServiceClient, 'getUser', stub);
-    const user = new User();
-    user.setName('users/test');
-    user.setDisplayName('Tommy');
-    await page.reload();
-    // stub.yield(null, user);
-    const cookiesSet = await page.cookies('http://localhost:3000/');
-    await fs.writeFile('test.txt', JSON.stringify(cookiesSet), () => {});
-    image = await page.screenshot();
-    expect(image).toMatchImageSnapshot();
-    await browser.close();
+    // let stub = sinon.stub();
+    // sinon.replace(mockUserServiceClient, 'getUser', stub);
+    // const user = new User();
+    // user.setName('users/test');
+    // user.setDisplayName('Tommy');
+    // await page.reload();
+    // // stub.yield(null, user);
+    // image = await page.screenshot();
+    // expect(image).toMatchImageSnapshot();
+    // await browser.close();
 });
